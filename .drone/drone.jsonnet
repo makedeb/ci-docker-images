93ca8251166b12aacafac137ccd47cda47f02520
Leo Puvilland <leo@craftcat.dev>
2022-01-27 21:18:44 -0800

    initial drone ci config

---
+++ .drone/drone.jsonnet
@@ -0,0 +1,27 @@
+local buildImage(source_image, target-tag) = {
+  name: "build-image-" + target_tag,
+  kind: "pipeline",
+  type: "docker",
+  volumes: [{name: "docker", host: {path: "/var/run/docker.sock"}}],
+  steps: [{
+    name: "build-and-publish-" + target_tag,
+    image: "docker",
+    volumes: [{name: "docker", path: "/var/run/docker.sock"}],
+    environment: {
+      proget_api_key: {from_secret: "proget_api_key"},
+      source_image: source_image,
+      target_tag: target_tag,
+    },
+    commands: [
+      "apk add --no-cache bash",
+      ".drone/scripts/main.sh"
+    ]
+  }]
+};
+
+
+[
+  buildImage("proget.hunterwittenborn.com/docker/makedeb/makedeb-alpha", "utests-alpha"),
+  buildImage("proget.hunterwittenborn.com/docker/makedeb/makedeb-beta", "utests-beta"),
+  buildImage("proget.hunterwittenborn.com/docker/makedeb/makedeb", "utests")
+]
---
+++ .drone/scripts/main.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -e
+
+# NOTE: This script needs to be ran from the root of the makedeb-docker
+# repository to function correctly.
+
+# Source functions
+for i in "run_verbosely" "configure_docker"; do
+  source ".drone/scripts/${i}.sh"
+done
+
+# Log in to ProGet server.
+configure_docker
+
+image="ci-image:${target_tag}"
+
+# Setup image.
+echo "[Info] Building '${image}' from '${source_image}'..."
+
+if [[ ! -f "${image}" ]]; then
+  echo "Unable to find dockerfile for ${image}"
+  exit 1
+fi
+
+sed -i "1s|{{image}}|${source_image}|" "./${target_dockerfile}"
+
+published_image_path="${proget_server}/docker/makedeb/${image}"
+
+# Build and publish image.
+docker build --no-cache \
+             --pull \
+             -t "${published_image_path}" \
+             -f "./${target_dockerfile}" \
+             --build-arg "proget_url=${proget_server}" \
+             ./
+
+docker push "${published_image_path}"
